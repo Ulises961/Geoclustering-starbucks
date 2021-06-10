5e3372bc81f6cf82f37bd3f35b97d51cb32c3b56
Sosa Ulises Emiliano <ulisesemiliano.sosa@eurac.edu>
2021-06-10 09:29:08 +0200

    Converter with the appender of sqmt added and the data set produced

--- Data/Csv-JsonConv.py
+++ Data/Csv-JsonConv.py
@@ -1,38 +1,51 @@
 import csv
 import json
-import random;
- 
-# Function to convert a CSV to JSON
-# Takes the file paths as arguments
+import random
+
 def make_json(csvFilePath, jsonFilePath):
    
-    # create a dictionary
+    # create a final dictionary, intermidiate output list
     data = {}
-     
+    intermediate_dataset=[]
+
     with open(csvFilePath, encoding='utf-8') as csvf:
-        csvReader = csv.DictReader(csvf)
+        csvReader = csv.reader(csvf)
          
-        # Convert each row into a dictionary
-        # and add it to data
+        # Append to each row a surface value (int)
+        
         for rows in csvReader:
              
-            # Column named 'Store Number' as
-            # the primary key
-            key = rows['Store Number']
-            rows.append(str(random.randrange(50,200)))
-            data[key] = rows
- 
+           
+            if csvReader.line_num == 1:
+                rows.append('Sqm')
+            else:
+                rows.append(str(random.randrange(50,200)))
+            
+            # Append to the intermediate data set the modified row
+            
+            intermediate_dataset.append(rows)
+        
+
+        # Convert each row into a dictionary
+        
+        for line in intermediate_dataset:
+               
+          if intermediate_dataset.index(line)!= 0:
+            key= line[1]
+            data[key] = line
+
+
     # Open a json writer, and use the json.dumps()
     # function to dump data
     with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
         jsonf.write(json.dumps(data, indent=4))
-         
+
+
 # Driver Code
  
-# Decide the two file paths according to your
-# computer system
-csvFilePath = r'mock-app/Data/directory.csv'
-jsonFilePath = r'mock-app/Data/directory.json'
+
+csvFilePath = r'Data/directory.csv'
+jsonFilePath = r'Data/directory.json'
  
 # Call the make_json function
 make_json(csvFilePath, jsonFilePath)
