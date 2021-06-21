0c508912cd6f54449b9a25c9aa23c537b9b4a7b2
Sosa Ulises Emiliano <ulisesemiliano.sosa@eurac.edu>
2021-06-21 16:00:07 +0200

    format refactoring

--- component/Layout/Layout.js
+++ component/Layout/Layout.js
@@ -1,41 +1,39 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+
 import Head from "next/dist/next-server/lib/head";
-import Navbar from './Navbar';
-export const siteTitle = 'K-Means';
+import Navbar from "./Navbar";
+export const siteTitle = "K-Means";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
+export default function Layout({ children, home, tab }) {
+  const classes = useStyles();
 
-export default function Layout({children, home ,tab}){
-
-    const classes = useStyles();
-    
-
-    return (
-            <>
-              <Head>
-                  <title>{siteTitle}</title>
-                  <link rel="icon" href="/favicon.ico" />
-                  <meta 
-                  name="description"
-                  content="Groupping Starbucks shops by their square surface" /> 
-                  <meta name="og:title" content={siteTitle} />
-              </Head>
-            <Navbar />
-            <main>{children}</main>
-            <footer> Created By ... </footer>
-          </>
-
-    );
+  return (
+    <>
+      <Head>
+        <title>{siteTitle}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Groupping Starbucks shops by their square surface"
+        />
+        <meta name="og:title" content={siteTitle} />
+      </Head>
+      <Navbar />
+      <main>{children}</main>
+      <footer> Created By ... </footer>
+    </>
+  );
 }
--- component/Markers/markers.js
+++ component/Markers/markers.js
@@ -1,28 +1,18 @@
+import MyMarker from "./Marker/marker";
 
-import MyMarker from './Marker/marker';
-
-const Markers = ({markers}) => {
-   // console.log(markers);
+const Markers = ({ markers }) => {
+  return markers.map((marker, index) => {
     return (
-        markers.map((marker,index)=> { 
-     
-            return <MyMarker
-                position= {[marker.Lat,marker.Lon]} 
-                color={marker.Color}
-                key = {[marker.Lat, marker.Lon,marker.Address],index}
-                addr = {marker.Address}
-                city = {marker.City}
-                meters = {marker.Sqmt}
-            
-                />
-            
-            })
-        
-        );
-
-    }
-
-
-
+      <MyMarker
+        position={[marker.Lat, marker.Lon]}
+        color={marker.Color}
+        key={([marker.Lat, marker.Lon, marker.Address], index)}
+        addr={marker.Address}
+        city={marker.City}
+        meters={marker.Sqmt}
+      />
+    );
+  });
+};
 
 export default Markers;
